c01abfd20adb06f375c8fb4a92d6c84a6e497e7b
Заневский Ромуальд <zanevskij_rp1_25@student.grsu.by>
2025-10-06 20:27:16 +0300

    Delete lab2 directory

--- lab2/A5.py
+++
@@ -1,7 +0,0 @@
-A = int(input('Введите число A: '))
-B = int(input('Введите число B: '))
-
-if A % B == 0:
-    print('YES')
-else:
-    print('NO')
--- lab2/A6.py
+++
@@ -1,7 +0,0 @@
-sec = int(input("Введите количество секунд с начала суток: "))
-
-hours = (sec // 3600) % 24
-minutes = (sec % 3600) // 60
-seconds = sec % 60
-
-print(f"{hours}:{minutes:02d}:{seconds:02d}")
