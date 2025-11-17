0ba2734cad2316a87fda37c484c1b05db4b6a112
Заневский Ромуальд <zanevskij_rp1_25@student.grsu.by>
2025-11-17 20:33:33 +0300

    Программа разбивает текст на предложения

--- lab5/A2.py
+++ lab5/A2.py
@@ -6,14 +6,14 @@ def main():
 
     for ch in text:
         sentence += ch
-        if ch in ".!?":   # конец предложения
+        if ch in ".!?": 
             sentences.append(sentence.strip())
             sentence = ""
 
-    # выводим каждое предложение
     for s in sentences:
         print(s)
 
     print("Предложений в тексте:", len(sentences))
 
 main()
+
